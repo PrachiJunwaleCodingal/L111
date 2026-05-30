cecc6904e35878b4ea8033472f67e1f1f5744d59
PrachiJunwaleCodingal <prachi.junwale@gmail.com>
2026-05-30 15:54:28 +0530

    Add files via upload

---
+++ A1.py
@@ -0,0 +1,23 @@
+
+num1 = 10
+num2 = 4
+# Using AND operator
+print("num1 & num2 =", num1 & num2)
+# Using OR operator
+print("\nnum1 | num2 =", num1 | num2)
+# Using NOT operator
+print("\n~num1 =", ~num1)
+# Using XOR operator
+print("\nnum1 ^ num2 =", num1 ^ num2)
+ 
+num1 = 10
+num2 = 4
+ #right shift will divide number by 2
+print("\nnum1 >> 1 =", num1 >> 1)
+print("\nnum2 >> 1 =", num2 >> 1)
+ 
+num1 = 10
+num2 = 4
+# Using Left Shift  will multiply by
+print("\nnum1 << 1 =", num1 << 1)
+print("\nnum2 << 1 =", num2 << 1)
---
+++ ACP.py
@@ -0,0 +1,14 @@
+# Program to solve the circuit given
+
+a=1
+b=0
+c=0
+
+aANDb = a & b
+bXORc = b ^ c
+bORc = b | c
+g = bXORc & bORc
+
+final = aANDb ^ g
+
+print("q = ",final)
